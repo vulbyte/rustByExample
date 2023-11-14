3f2466e1322425513cd01a89ebc3a706e306ecb5
vulbyte <insertanimationnamehere@gmail.com>
2023-11-13 20:31:36 -0800

    completed 5_4

---
+++ 5-types/5_4-aliasing.rs
@@ -0,0 +1,23 @@
+//the type statement can be used to give a new name to an existing type. Types must have
+//UpperCamelCase names, or the compiler will raise a warning. The exception to this rule are the
+//primitive types: usize, f32, etc
+
+type NanoSecond = u64;
+type Inch = u64;
+type U64 = u64;
+
+fn main() {
+    //ns = in = u64 = u64
+    //the : is pre declarign the variable as a type that uses x data type
+    let nanoseconds: NanoSecond = 5 as U64;
+    let inches: Inch = 2 as U64;
+
+    // not that type aliases *dont* provide any extra type safety, because aliases are *not* new
+    // types
+    println!(
+        "{} nanoseconds + {} inches + {} unit?",
+        nanoseconds,
+        inches,
+        nanoseconds + inches
+    );
+}
